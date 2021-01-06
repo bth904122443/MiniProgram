f2a869ca99a7b9e30beae036567d246331b4077e
WangJing <741455248@qq.com>
2021-01-06 22:38:33 +0800

    MusicDemo

--- MusicDemo/pages/posts/posts.js
+++ MusicDemo/pages/posts/posts.js
@@ -12,7 +12,31 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var content = [{
+      title:"2020LPL夏季赛观后感",
+      content:"8月9好，LPL常规赛收官之战结束，在使馆赛后赛伦茨的比赛中关键对局中，SN战胜了FPX，为本赛季常规赛画上句号",
+      dateTime:"24小时前",
+      headImgSrc:"/images/article-1.jpg",
+      avatar:"/images/avatar.jpg",
+      date:"Nov 20 2020",
+      reading:102,
+      collection:92,
+      postId:3,
+    },
+    {
+      title:"2020LPL夏季赛观后感",
+      content:"8月9好，LPL常规赛收官之战结束，在使馆赛后赛伦茨的比赛中关键对局中，SN战胜了FPX，为本赛季常规赛画上句号",
+      dateTime:"24小时前",
+      headImgSrc:"/images/article-1.jpg",
+      avatar:"/images/avatar.jpg",
+      date:"Nov 20 2020",
+      reading:102,
+      collection:92,
+      postId:3,
+    }];
+    this.setData({
+      posts:content
+    })
   },
 
   /**
