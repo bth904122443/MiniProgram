d9afb3d982e0592b69955f5f754883a9cfee1241
WangJing <741455248@qq.com>
2021-01-11 23:13:12 +0800

    添加tabBar

--- MusicDemo/data/data.js
+++ MusicDemo/data/data.js
@@ -22,7 +22,7 @@ var postList = [{
   author:"charon",
   dateTime:"24小时前",
   detail:"8月9好，LPL常规赛收官之战结束，在使馆赛后赛伦茨的比赛中关键对局中，SN战胜了FPX，为本赛季常规赛画上句号",
-  headImgSrc:"/images/article-1.jpg",
+  headImgSrc:"/images/article-2.jpg",
   avatar:"/images/avatar.jpg",
   date:"Nov 20 2020",
   reading:102,
@@ -33,6 +33,25 @@ var postList = [{
       title: "笑看风云",
       coverImg: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002sNbWp3royJG_1.jpg?max_age=2592000"
   }
-}];
+},
+{
+  title:"2020LPL夏季赛观后感",
+  content:"8月9好，LPL常规赛收官之战结束，在使馆赛后赛伦茨的比赛中关键对局中，SN战胜了FPX，为本赛季常规赛画上句号",
+  author:"charon",
+  dateTime:"24小时前",
+  detail:"8月9好，LPL常规赛收官之战结束，在使馆赛后赛伦茨的比赛中关键对局中，SN战胜了FPX，为本赛季常规赛画上句号",
+  headImgSrc:"/images/article-3.jpg",
+  avatar:"/images/avatar.jpg",
+  date:"Nov 20 2020",
+  reading:102,
+  collection:92,
+  postId:2,
+  music: {
+    url: "http://music.163.com/song/media/outer/url?id=1386866050.mp3",
+      title: "笑看风云",
+      coverImg: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002sNbWp3royJG_1.jpg?max_age=2592000"
+  }
+}
+];
 
 export {postList}
